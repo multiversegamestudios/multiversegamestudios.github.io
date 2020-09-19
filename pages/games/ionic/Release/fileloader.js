c92e7483a1eefe934d19b51ac8b63bb9b266fada
John Carter <jfcarter2358@gmail.com>
2020-09-19 11:14:28 -0500

    Add WebGL Games

---
+++ pages/games/ionic/Release/fileloader.js
@@ -0,0 +1,208 @@
+
+var Module;
+if (typeof Module === 'undefined') Module = eval('(function() { try { return Module || {} } catch(e) { return {} } })()');
+if (!Module.expectedDataFileDownloads) {
+  Module.expectedDataFileDownloads = 0;
+  Module.finishedDataFileDownloads = 0;
+}
+Module.expectedDataFileDownloads++;
+(function() {
+
+    var PACKAGE_PATH;
+    if (typeof window === 'object') {
+      PACKAGE_PATH = window['encodeURIComponent'](window.location.pathname.toString().substring(0, window.location.pathname.toString().lastIndexOf('/')) + '/');
+    } else {
+      // worker
+      PACKAGE_PATH = encodeURIComponent(location.pathname.toString().substring(0, location.pathname.toString().lastIndexOf('/')) + '/');
+    }
+    var PACKAGE_NAME = 'Ionic Build 1.4.data';
+    var REMOTE_PACKAGE_BASE = 'Ionic Build 1.4.data';
+    if (typeof Module['locateFilePackage'] === 'function' && !Module['locateFile']) {
+      Module['locateFile'] = Module['locateFilePackage'];
+      Module.printErr('warning: you defined Module.locateFilePackage, that has been renamed to Module.locateFile (using your locateFilePackage for now)');
+    }
+    var REMOTE_PACKAGE_NAME = typeof Module['locateFile'] === 'function' ?
+                              Module['locateFile'](REMOTE_PACKAGE_BASE) :
+                              ((Module['filePackagePrefixURL'] || '') + REMOTE_PACKAGE_BASE);
+    var REMOTE_PACKAGE_SIZE = 7935865;
+    var PACKAGE_UUID = '05e23789-074d-41c7-8b74-b8df42bad507';
+  
+    function fetchRemotePackage(packageName, packageSize, callback, errback) {
+      var xhr = new XMLHttpRequest();
+      xhr.open('GET', packageName, true);
+      xhr.responseType = 'arraybuffer';
+      xhr.onprogress = function(event) {
+        var url = packageName;
+        var size = packageSize;
+        if (event.total) size = event.total;
+        if (event.loaded) {
+          if (!xhr.addedTotal) {
+            xhr.addedTotal = true;
+            if (!Module.dataFileDownloads) Module.dataFileDownloads = {};
+            Module.dataFileDownloads[url] = {
+              loaded: event.loaded,
+              total: size
+            };
+          } else {
+            Module.dataFileDownloads[url].loaded = event.loaded;
+          }
+          var total = 0;
+          var loaded = 0;
+          var num = 0;
+          for (var download in Module.dataFileDownloads) {
+          var data = Module.dataFileDownloads[download];
+            total += data.total;
+            loaded += data.loaded;
+            num++;
+          }
+          total = Math.ceil(total * Module.expectedDataFileDownloads/num);
+          if (Module['setStatus']) Module['setStatus']('Downloading data... (' + loaded + '/' + total + ')');
+        } else if (!Module.dataFileDownloads) {
+          if (Module['setStatus']) Module['setStatus']('Downloading data...');
+        }
+      };
+      xhr.onload = function(event) {
+        var packageData = xhr.response;
+        callback(packageData);
+      };
+      xhr.send(null);
+    };
+
+    function handleError(error) {
+      console.error('package error:', error);
+    };
+  
+      var fetched = null, fetchedCallback = null;
+      fetchRemotePackage(REMOTE_PACKAGE_NAME, REMOTE_PACKAGE_SIZE, function(data) {
+        if (fetchedCallback) {
+          fetchedCallback(data);
+          fetchedCallback = null;
+        } else {
+          fetched = data;
+        }
+      }, handleError);
+    
+  function runWithFS() {
+
+function assert(check, msg) {
+  if (!check) throw msg + new Error().stack;
+}
+Module['FS_createPath']('/', 'Resources', true, true);
+
+    function DataRequest(start, end, crunched, audio) {
+      this.start = start;
+      this.end = end;
+      this.crunched = crunched;
+      this.audio = audio;
+    }
+    DataRequest.prototype = {
+      requests: {},
+      open: function(mode, name) {
+        this.name = name;
+        this.requests[name] = this;
+        Module['addRunDependency']('fp ' + this.name);
+      },
+      send: function() {},
+      onload: function() {
+        var byteArray = this.byteArray.subarray(this.start, this.end);
+
+          this.finish(byteArray);
+
+      },
+      finish: function(byteArray) {
+        var that = this;
+        Module['FS_createPreloadedFile'](this.name, null, byteArray, true, true, function() {
+          Module['removeRunDependency']('fp ' + that.name);
+        }, function() {
+          if (that.audio) {
+            Module['removeRunDependency']('fp ' + that.name); // workaround for chromium bug 124926 (still no audio with this, but at least we don't hang)
+          } else {
+            Module.printErr('Preloading file ' + that.name + ' failed');
+          }
+        }, false, true); // canOwn this data in the filesystem, it is a slide into the heap that will never change
+        this.requests[this.name] = null;
+      },
+    };
+      new DataRequest(0, 10176, 0, 0).open('GET', '/level0');
+    new DataRequest(10176, 32880, 0, 0).open('GET', '/level1');
+    new DataRequest(32880, 48048, 0, 0).open('GET', '/level2');
+    new DataRequest(48048, 59096, 0, 0).open('GET', '/level3');
+    new DataRequest(59096, 70144, 0, 0).open('GET', '/level4');
+    new DataRequest(70144, 82992, 0, 0).open('GET', '/level5');
+    new DataRequest(82992, 94560, 0, 0).open('GET', '/level6');
+    new DataRequest(94560, 105776, 0, 0).open('GET', '/level7');
+    new DataRequest(105776, 116000, 0, 0).open('GET', '/level8');
+    new DataRequest(116000, 149232, 0, 0).open('GET', '/mainData');
+    new DataRequest(149232, 149253, 0, 0).open('GET', '/methods_pointedto_by_uievents.xml');
+    new DataRequest(149253, 160413, 0, 0).open('GET', '/resources.assets');
+    new DataRequest(160413, 2659129, 0, 0).open('GET', '/sharedassets0.assets');
+    new DataRequest(2659129, 3243749, 0, 0).open('GET', '/sharedassets0.resource');
+    new DataRequest(3243749, 3248061, 0, 0).open('GET', '/sharedassets1.assets');
+    new DataRequest(3248061, 3606185, 0, 0).open('GET', '/sharedassets2.assets');
+    new DataRequest(3606185, 3610497, 0, 0).open('GET', '/sharedassets3.assets');
+    new DataRequest(3610497, 4389425, 0, 0).open('GET', '/sharedassets4.assets');
+    new DataRequest(4389425, 4393785, 0, 0).open('GET', '/sharedassets5.assets');
+    new DataRequest(4393785, 4398145, 0, 0).open('GET', '/sharedassets6.assets');
+    new DataRequest(4398145, 4402505, 0, 0).open('GET', '/sharedassets7.assets');
+    new DataRequest(4402505, 4406865, 0, 0).open('GET', '/sharedassets8.assets');
+    new DataRequest(4406865, 5861385, 0, 0).open('GET', '/sharedassets9.assets');
+    new DataRequest(5861385, 7421757, 0, 0).open('GET', '/Resources/unity_default_resources');
+    new DataRequest(7421757, 7935865, 0, 0).open('GET', '/Resources/unity_builtin_extra');
+
+    function processPackageData(arrayBuffer) {
+      Module.finishedDataFileDownloads++;
+      assert(arrayBuffer, 'Loading data file failed.');
+      var byteArray = new Uint8Array(arrayBuffer);
+      var curr;
+      
+      // Reuse the bytearray from the XHR as the source for file reads.
+      DataRequest.prototype.byteArray = byteArray;
+          DataRequest.prototype.requests["/level0"].onload();
+          DataRequest.prototype.requests["/level1"].onload();
+          DataRequest.prototype.requests["/level2"].onload();
+          DataRequest.prototype.requests["/level3"].onload();
+          DataRequest.prototype.requests["/level4"].onload();
+          DataRequest.prototype.requests["/level5"].onload();
+          DataRequest.prototype.requests["/level6"].onload();
+          DataRequest.prototype.requests["/level7"].onload();
+          DataRequest.prototype.requests["/level8"].onload();
+          DataRequest.prototype.requests["/mainData"].onload();
+          DataRequest.prototype.requests["/methods_pointedto_by_uievents.xml"].onload();
+          DataRequest.prototype.requests["/resources.assets"].onload();
+          DataRequest.prototype.requests["/sharedassets0.assets"].onload();
+          DataRequest.prototype.requests["/sharedassets0.resource"].onload();
+          DataRequest.prototype.requests["/sharedassets1.assets"].onload();
+          DataRequest.prototype.requests["/sharedassets2.assets"].onload();
+          DataRequest.prototype.requests["/sharedassets3.assets"].onload();
+          DataRequest.prototype.requests["/sharedassets4.assets"].onload();
+          DataRequest.prototype.requests["/sharedassets5.assets"].onload();
+          DataRequest.prototype.requests["/sharedassets6.assets"].onload();
+          DataRequest.prototype.requests["/sharedassets7.assets"].onload();
+          DataRequest.prototype.requests["/sharedassets8.assets"].onload();
+          DataRequest.prototype.requests["/sharedassets9.assets"].onload();
+          DataRequest.prototype.requests["/Resources/unity_default_resources"].onload();
+          DataRequest.prototype.requests["/Resources/unity_builtin_extra"].onload();
+          Module['removeRunDependency']('datafile_Ionic Build 1.4.data');
+
+    };
+    Module['addRunDependency']('datafile_Ionic Build 1.4.data');
+  
+    if (!Module.preloadResults) Module.preloadResults = {};
+  
+      Module.preloadResults[PACKAGE_NAME] = {fromCache: false};
+      if (fetched) {
+        processPackageData(fetched);
+        fetched = null;
+      } else {
+        fetchedCallback = processPackageData;
+      }
+    
+  }
+  if (Module['calledRun']) {
+    runWithFS();
+  } else {
+    if (!Module['preRun']) Module['preRun'] = [];
+    Module["preRun"].push(runWithFS); // FS is not initialized yet, wait for it
+  }
+
+})();
